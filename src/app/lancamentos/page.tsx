b29056cff5f2a60bff414b4973b8436945142cfa
Ramon Martins Ferreira <ramon7mf@gmail.com>
2025-12-16 20:05:20 -0300

    Update page.tsx

--- src/app/lancamentos/page.tsx
+++ src/app/lancamentos/page.tsx
@@ -10,10 +10,10 @@ export const dynamic = 'force-dynamic';
 let API_BASE_URL: string | undefined = undefined;
 
 if (process.env.NEXT_PUBLIC_ENVIRONMENT == 'production') {
-    API_BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL;
+    API_BASE_URL = process.env.API_INTERNAL_URL;
     API_BASE_URL = `https://${API_BASE_URL}/lancamentos/listar_todos`;
 } else {
-    API_BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL;
+    API_BASE_URL = process.env.API_LOCAL_URL;
     API_BASE_URL = `${API_BASE_URL}/lancamentos/listar_todos`;
 }
 
