76a066556284ad286ec7b7553daf44fa839cf145
Ramon Martins Ferreira <ramon7mf@gmail.com>
2026-01-10 03:18:14 -0300

    fix: Ajuste CSS index

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,18 +10,21 @@ export const metadata: Metadata = {
 
 export default function HomePage() {
 	return (
-		<div className="bg-gray-100 flex flex-col items-center min-h-screen pt-5 gap-5">
-			<div className='bg-(--color_primary_default) flex flex-row items-center justify-between navbar-index-width w-full rounded-sm shadow-lg'>
-				<Logo />
-				<NavbarMenu />
-			</div>
-			<h1 className="text-4xl font-extrabold mt-12 text-gray-700">
-				Bem-vindo ao Ferreira Finanças
-			</h1>
+		<div className="bg-gray-100 flex flex-col items-center min-h-screen pt-5">
+			<div className="flex flex-col justify-center items-center navbar-index-width gap-5">
+				<div className='bg-(--color_primary_default) flex flex-row py-0.5 px-2.5 items-center justify-between w-full rounded-sm shadow-lg'>
+					<Logo />
+					<NavbarMenu />
+				</div>
+				<h1 className="text-4xl font-extrabold mt-12 text-gray-700">
+					Bem-vindo ao Ferreira Finanças
+				</h1>
 
-			<p className="text-xl text-gray-500">
-				Suas finanças de forma simplificada e completa.
-			</p>
+				<p className="text-xl text-gray-500">
+					Suas finanças de forma simplificada e completa.
+				</p>
+			</div>
+			
 		</div>
 	);
 }
