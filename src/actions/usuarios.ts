531876139f5422d7709e8d32e851c34a74de867b
Ramon Martins Ferreira <ramon7mf@gmail.com>
2026-01-10 21:22:57 -0300

    refact: Add tratativa para limpar cookies

--- src/actions/usuarios.ts
+++ src/actions/usuarios.ts
@@ -3,6 +3,7 @@
 "use server"
 
 import { client_axios } from "@/lib/axios"
+import { cookies } from "next/headers";
 
 // ####################
 // ==> Action para obter os dados do usuário logado
@@ -15,6 +16,12 @@ export async function usuarioAtual() {
             error: null
         };
     } catch (error: any) {
+        // Se o navegador enviou um cookie que a API não aceitou mais (401), ele é limpo
+        if (error.response?.status === 401) {
+            const cookieStore = await cookies();
+            cookieStore.delete("auth_token");
+        }
+
         return {
             usuario: null,
             error: error.response?.data?.detail || "Erro ao carregar usuário"
