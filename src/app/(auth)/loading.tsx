8517d3f23c7442fb9279e86fbc0e9f23913ef08f
Ramon Martins Ferreira <ramon7mf@gmail.com>
2026-01-23 13:13:57 -0300

    fix: pagina de loading geral

--- src/app/(auth)/loading.tsx
+++ src/app/(auth)/loading.tsx
@@ -1,8 +1,16 @@
+// src/app/loading.tsx
+import { LoaderCircle } from "lucide-react";
 
-import LoadingRotas from "@/components/general/LoadingRotas";
 
 export default function Loading() {
     return (
-        <LoadingRotas />
+        <div className="fixed inset-0 z-50 flex flex-col items-center justify-center bg-gray-50/50 backdrop-blur-sm">
+            <div className="flex flex-col items-center gap-4">
+                <LoaderCircle className="h-12 w-12 animate-spin text-(--color_primary_default)" />
+                <p className="font-rochester text-(--color_primary_default) text-xl animate-pulse">
+                    Ferreira Finanças
+                </p>
+            </div>
+        </div>
     );
 }
